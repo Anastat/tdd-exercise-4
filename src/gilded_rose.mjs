1313652bbe33f1bcb1b629df1393152d26527de7
anastat <anastasiia.tikhonova@mail.com>
2025-04-07 20:47:22 +0300

    Refactoring: Introduced const values.

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -1,3 +1,7 @@
+const AGED_BRIE = "Aged Brie"
+const TAFKAL80ETC = "Backstage passes to a TAFKAL80ETC concert"
+const SULFARUS = "Sulfuras, Hand of Ragnaros"
+
 export class Item {
   constructor(name, sellIn, quality) {
     this.name = name;
@@ -13,16 +17,16 @@ export class Shop {
 
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+      if (this.items[i].name != AGED_BRIE && this.items[i].name != TAFKAL80ETC) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+          if (this.items[i].name != SULFARUS) {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+          if (this.items[i].name == TAFKAL80ETC) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -36,14 +40,14 @@ export class Shop {
           }
         }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+      if (this.items[i].name != SULFARUS) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+        if (this.items[i].name != AGED_BRIE) {
+          if (this.items[i].name != TAFKAL80ETC) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+              if (this.items[i].name != SULFARUS) {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
