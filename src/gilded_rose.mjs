42c8abb727996f7e301497904583fe8d831b8a7c
anastat <anastasiia.tikhonova@mail.com>
2025-04-07 21:12:59 +0300

    Refactoring: Replaced if/else conditions.

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -38,7 +38,11 @@ export class Shop {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != AGED_BRIE) {
+        if (this.items[i].name == AGED_BRIE) {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1;
+          }
+        } else {
           if (this.items[i].name != TAFKAL80ETC) {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != SULFARUS) {
@@ -47,10 +51,6 @@ export class Shop {
             }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
           }
         }
       }
